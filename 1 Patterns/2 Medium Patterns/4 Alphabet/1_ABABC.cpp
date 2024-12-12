5b9495eac51bbf01c1f12828d86b87161030154e
neeshuyadav2410 <neeshuyadav2410@gmail.com>
2024-12-12 21:44:05 +0530

    h

--- 1 Patterns/2 Medium Patterns/4 Alphabet/1_ABABC.cpp
+++ 1 Patterns/2 Medium Patterns/4 Alphabet/1_ABABC.cpp
@@ -1,12 +1,26 @@
 #include<iostream>
 using namespace std;
 int main(){
+
     int n;
+    
     cout<<"Enter n: ";
     cin>>n;
 
-    cout<<char('A'+3);
+    for(int i=0; i<n; i++){
+        char ch='A'-1;
+        for(int j=0; j<=i; j++){
+            ch++;
+            cout<<ch;
+        }
 
+        for(int k=0; k<i; k++){
+            ch--;
+            cout<<ch;
+        }
+
+        cout<<endl;
+    }
     
     return 0;
 }
